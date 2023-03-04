93b78f4ba1b079f2f36cca9fd900474b83c88860
Walter Mach <owaldovelasz@gmail.com>
2023-03-03 21:01:30 -0600

    Add see more button on the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import { OutlineButton } from '../components/Button/Button';
 import HeroSlide from '../containers/HeroSlide/HeroSlide';
 
 function Home() {
-    return <HeroSlide />;
+    return (
+        <>
+            <HeroSlide />
+            <div className="container">
+                <div className="section mb-3">
+                    <div className="section__header mb2">
+                        <h2>Trending</h2>
+                        <Link to="/movie">
+                            <OutlineButton cssClass="small" onClick={() => {}}>
+                                View More
+                            </OutlineButton>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Home;
